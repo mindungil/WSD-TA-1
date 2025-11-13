2caa6edf023328bcd6a5ffd7cca1ed4cd06dd3f5
mindungil <rlfalswns12@gmail.com>
2025-11-14 02:43:03 +0900

    hotfix: helmet 라이브러리 제거

--- src/server.js
+++ src/server.js
@@ -29,10 +29,7 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
   credentials: false
 }));
-app.use(helmet({
-  crossOriginResourcePolicy: { policy: "cross-origin" },
-  crossOriginEmbedderPolicy: false
-}));
+
 app.use(express.json({limit: '10mb'}));
 app.use(express.urlencoded({extended: true}));
 
